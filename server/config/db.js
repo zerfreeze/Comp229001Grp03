65463c2eb6e5d3a3f5e86586e0ee4073eee17d4e
zerfreeze <yormeno@gmail.com>
2023-03-04 00:01:01 -0500

    First release

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI":"mongodb+srv://rormenol:uXR8I7ARASwDGaiz@cluster0.kcyopy1.mongodb.net/?retryWrites=true&w=majority"
+
+};
